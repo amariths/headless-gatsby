2107ba612d54533321319568ccedcf3f44102b6f
amariths <amar.mehdi@iths.se>
2024-01-11 17:56:33 +0100

    a

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,13 @@
  */
 
 // You can delete this file if you're not using it
+
+// gatsby-browser.js
+import React from 'react';
+import { isRTL } from './src/utils/helpers';
+
+export const wrapRootElement = ({ element }) => {
+  const dir = isRTL() ? 'rtl' : 'ltr'; // Set dynamically based on your logic
+  document.documentElement.setAttribute('dir', dir);
+  return element;
+};
